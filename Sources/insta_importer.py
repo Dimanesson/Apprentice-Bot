5f4a8c23d966aa477053104498c0387149dd6b66
Dmitry <dimanessontc@gmail.com>
2020-11-22 17:00:40 +0300

    * instagram video importing updated

--- Sources/insta_importer.py
+++ Sources/insta_importer.py
@@ -1,33 +1,6 @@
 import requests
-import json
-import uuid
-import os
 
-class auto_deleting_file(object):
-    def __init__(self, path : str, title : str):
-        self.title = title
-        self.path = path
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        os.remove(self.path)
-
-    path : str
-    title : str
-
-
-def download(url, local_filename):
-    r = requests.get(url, stream=True)
-    with open(os.getcwd()+"/"+local_filename, 'wb') as f:
-        for chunk in r.iter_content(1024):
-            if chunk:
-                f.write(chunk)
-                f.flush()
-    return local_filename
-
-def import_video(url : str) -> auto_deleting_file:
+def import_video(url : str) -> {}:
     r = requests.get(url, params={'__a': 1})
     headers = r.headers['Content-type']
     rjson = r.json()
@@ -36,14 +9,16 @@ def import_video(url : str) -> auto_deleting_file:
         (not 'graphql' in rjson)
     ):
         raise Exception('Wrong link')
-    
-    with open("media.json", "w") as file:
-        file.write(json.dumps(rjson, indent=4, sort_keys=True))
 
     media = rjson['graphql']['shortcode_media']
     if media['is_video']:
-        return auto_deleting_file(download(media["video_url"], str(uuid.uuid4()) + ".mp4"), media["title"])
+        return {
+            "url" : media["video_url"],
+            "title" : f"{media['title']} {media['edge_media_to_caption']['edges'][0]['node']['text']}",
+            "preview" : media["display_resources"][0]["src"],
+            "description" : media["owner"]["full_name"],
+            "username" : media["owner"]["username"],
+            "usericon" : media["owner"]["profile_pic_url"]
+            }
     else:
         raise Exception("The post is not a video")
-
-    # application/json; charset=utf-8
--- Sources/main.py
+++ Sources/main.py
@@ -30,9 +30,11 @@ async def register(ctx, arg):
 @bot.command()
 async def instagram(ctx : discord.ext.commands.Context, arg):
     try:
-        with insta_importer.import_video(arg) as video_data:
-            file = discord.File(video_data.path, video_data.title)
-            await ctx.send(content=video_data.title, file=file)
+        video = insta_importer.import_video(arg)
+        emb = discord.Embed(title=video["title"], description=video["description"], url=video["url"])
+        emb.set_image(url=video["preview"])
+        emb.set_author(name=video['username'], url=f"https://instagram.com/{video['username']}", icon_url=video["usericon"])
+        await ctx.send(embed=emb)
 
     except Exception as e:
         await ctx.send(f"Sorry, no such video  {e}")
