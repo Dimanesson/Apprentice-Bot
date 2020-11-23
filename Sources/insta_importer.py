d03f1a8ff70f483df8d4e329487a24b6e3a7b489
Dmitry <dimanessontc@gmail.com>
2020-11-23 20:42:43 +0300

    * user-agent changed * Code formatting

--- Sources/insta_importer.py
+++ Sources/insta_importer.py
@@ -4,12 +4,13 @@ import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.poolmanager import PoolManager
 
-__session__ : requests.Session
+__session__: requests.Session
 
-def import_video(url : str) -> {}:
-    u_a = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.82 Safari/537.36"
+
+def import_video(url: str) -> {}:
+    u_a = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
     print("Performing request")
-    r = __session__.get(url, params={'__a': 1, "USER-AGENT":u_a})
+    r = __session__.get(url, params={'__a': 1, "user-agent": u_a})
     print("Got request")
 
     headers = r.headers['Content-type']
@@ -24,7 +25,7 @@ def import_video(url : str) -> {}:
         raise Exception('Wrong link')
 
     print("Link confirmed")
-    
+
     print(r.json())
     print("Dumped JSON")
 
@@ -34,18 +35,20 @@ def import_video(url : str) -> {}:
 
     if media['is_video']:
         return {
-            "url" : media["video_url"],
-            "title" : f"{media['title']} {media['edge_media_to_caption']['edges'][0]['node']['text']}",
-            "preview" : media["display_resources"][0]["src"],
-            "description" : media["owner"]["full_name"],
-            "username" : media["owner"]["username"],
-            "usericon" : media["owner"]["profile_pic_url"]
-            }
+            "url": media["video_url"],
+            "title": f"{media['title']} {media['edge_media_to_caption']['edges'][0]['node']['text']}",
+            "preview": media["display_resources"][0]["src"],
+            "description": media["owner"]["full_name"],
+            "username": media["owner"]["username"],
+            "usericon": media["owner"]["profile_pic_url"]
+        }
     else:
         raise Exception("The post is not a video")
 
+
 class SourcePortAdapter(HTTPAdapter):
     """"Transport adapter" that allows us to set the source port."""
+
     def __init__(self, port, *args, **kwargs):
         self._source_port = port
         super(SourcePortAdapter, self).__init__(*args, **kwargs)
@@ -61,6 +64,7 @@ def init(port):
     __session__ = requests.Session()
     __session__.mount('http://', SourcePortAdapter(port))
     __session__.mount('https://', SourcePortAdapter(port))
+
 
 def unit():
     global __session__
